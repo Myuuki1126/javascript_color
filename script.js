ea87d3b299c63bfe4946c56a174aa96c55fbb4ed
Myuuki1126 <109127643+Myuuki1126@users.noreply.github.com>
2023-09-24 23:20:03 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+// JavaScript
+const color = document.getElementById('myColor');
+const text = document.getElementById('bodyText');
+//console.log(document.getElementById('myColor').value);
+text.textContent = color.value;
+function changeColor(){
+    // カラーコードを表示
+      text.textContent = color.value;
+      // 背景色を変更
+  document.body.style.backgroundColor = color.value;
+    }
+// カラーピッカーで色が選択されたら、changeColorを動作させる
+color.addEventListener('input', changeColor);
